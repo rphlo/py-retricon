b3f2339337f72f36a1f172200482de85bca0203b
Raphaël Stefanini <rphl@rphl.net>
2014-06-02 18:23:22 +0000

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 import os
 from setuptools import setup
 
-README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
 
 setup(
     name='retricon',
     version='1.0.0',
     packages=['retricon',],
     license="MIT License",
-    description='Library to create cool avatar',
+    description='Library to create unique images such as Github avatar',
     long_description=README,
     url='http://github.com/rphlo/django-seuranta',
     download_url='https://github.com/rphlo/py-retricon/tarball/v1.0.0',
