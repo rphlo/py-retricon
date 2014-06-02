ec67dd1d1a9a6bd47ff480279437fea512020116
Raphaël Stefanini <raphael.stefanini@qvantel.com>
2014-06-02 14:47:44 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+README = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='retricon',
