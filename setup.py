b739da1ee3f54ba60bb2d86cb452c7ff0ab05c0d
Raphaël Stefanini <rphl@rphl.net>
2014-07-04 18:14:01 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='retricon',
-    version='1.0.3',
+    version='1.1.0',
     packages=['retricon',],
     license="MIT License",
     description='Library to create unique images such as Github avatar',
     url='http://github.com/rphlo/py-retricon',
-    download_url='https://github.com/rphlo/py-retricon/tarball/v1.0.3',
+    download_url='https://github.com/rphlo/py-retricon/tarball/v1.1.0',
     author='Raphael Stefanini',
     author_email='rphl@rphl.net',
     keywords = ['image', 'avatar', 'icon'],
