bc2986d49b029c27c269c295785d59e9e8128c6d
Raphaël Stefanini <rphl@rphl.net>
2015-09-07 05:26:48 +0300

    PEP-8 spacing

--- retricon/retricon.py
+++ retricon/retricon.py
@@ -8,7 +8,7 @@ from six import string_types
 
 
 def brightness(r, g, b):
-    return math.sqrt(.241*r*r+.691*g*g+.068*b*b)
+    return math.sqrt(.241 * r * r + .691 * g * g + .068 * b * b)
 
 
 def key_brightness(a):
@@ -17,30 +17,30 @@ def key_brightness(a):
 
 def fixed_length_hash(buf, length):
     if length > 64:
-        raise Exception('sha512 can only generate 64B of data:'
-                        ' %dB requested' % length)
-    hex_length = length*2
+        msg = 'sha512 can only generate 64B of data: {}B requested'
+        raise Exception(msg.format(length))
+    hex_length = length * 2
     val = hashlib.sha512(buf).hexdigest()
     if len(val) % hex_length != 0:
-        val += "0"*(hex_length-len(val) % hex_length)
+        val += "0" * (hex_length - len(val) % hex_length)
     ii = hex_length
     ret = val[0:ii]
     while ii < len(val):
-        ret = format(int(ret, 16) ^ int(val[ii:ii+hex_length], 16), 'x')
+        ret = format(int(ret, 16) ^ int(val[ii:ii + hex_length], 16), 'x')
         ii += hex_length
     if len(ret) < hex_length:
-        ret = "0"*(hex_length-len(ret)) + ret
+        ret = "0" * (hex_length - len(ret)) + ret
     return codecs.decode(ret, 'hex')
 
 
 def id_hash(name, length, min_fill, max_fill, use_colors):
-    buf = name+" "
+    buf = name + " "
     buf_bytes = str.encode(buf)
-    needed_bytes = int(math.ceil(length/8.0))
+    needed_bytes = int(math.ceil(length / 8.0))
     if use_colors:
         needed_bytes += 6
     for ii in range(0x100):
-        buf_bytes = buf_bytes[:-1]+struct.pack('B', ii)
+        buf_bytes = buf_bytes[:-1] + struct.pack('B', ii)
         fp = fixed_length_hash(buf_bytes, needed_bytes)
         fp = [struct.unpack('B', fp[ii:ii+1])[0] for ii, x in enumerate(fp)]
         pixels = []
@@ -64,14 +64,14 @@ def id_hash(name, length, min_fill, max_fill, use_colors):
                 'colors': colors,
                 'pixels': pixels
             }
-    raise Exception("String '''%s''' unhashable in"
-                    " single-byte search space." % name)
+    raise Exception("String `{}` unhashable in single-byte search"
+                    " space.".format(name))
 
 
 def fill_pixels(raw, dimension):
-    pic = [None]*dimension
+    pic = [None] * dimension
     for row in range(dimension):
-        pic[row] = [None]*dimension
+        pic[row] = [None] * dimension
         for col in range(dimension):
             ii = row * dimension + col
             pic[row][col] = raw['pixels'][ii]
@@ -81,9 +81,9 @@ def fill_pixels(raw, dimension):
 def fill_pixels_vert_sym(raw, dimension):
     mid = int(math.ceil(dimension / 2.0))
     odd = dimension % 2 != 0
-    pic = [None]*dimension
+    pic = [None] * dimension
     for row in range(dimension):
-        pic[row] = [None]*dimension
+        pic[row] = [None] * dimension
         for col in range(dimension):
             if col < mid:
                 ii = row * mid + col
@@ -100,9 +100,9 @@ def fill_pixels_vert_sym(raw, dimension):
 def fill_pixels_cent_sym(raw, dimension):
     mid = int(math.ceil(dimension / 2.0))
     odd = dimension % 2 != 0
-    pic = [None]*dimension
+    pic = [None] * dimension
     for row in range(dimension):
-        pic[row] = [None]*dimension
+        pic[row] = [None] * dimension
         for col in range(dimension):
             if col >= mid:
                 dist_middle = mid - col
@@ -125,9 +125,9 @@ def fill_pixels_cent_sym(raw, dimension):
 
 def fill_pixels_hori_sym(raw, dimension):
     mid = int(math.ceil(dimension / 2.0))
-    pic = [None]*dimension
+    pic = [None] * dimension
     for row in range(dimension):
-        pic[row] = [None]*dimension
+        pic[row] = [None] * dimension
         for col in range(dimension):
             if row < mid:
                 ii = (row * dimension) + col
@@ -209,40 +209,42 @@ def retricon(name, tiles=5, tile_size=1, tile_color=0, bg_color=None,
         use_color = True
     else:
         use_color = False
-    tile_width = tile_size+tile_padding*2
-    canvas_size = tile_width*tiles+image_padding*2
-    draw_scale = max((width//canvas_size), 1)
+    tile_width = tile_size + tile_padding * 2
+    canvas_size = tile_width * tiles + image_padding * 2
+    draw_scale = max((width // canvas_size), 1)
     dimension = tiles
-    mid = int(math.ceil(dimension/2.0))
+    mid = int(math.ceil(dimension / 2.0))
     if vertical_sym and horizontal_sym:
-        raw = id_hash(name, mid*mid, min_fill, max_fill, use_color)
+        raw = id_hash(name, mid * mid, min_fill, max_fill, use_color)
         pic = fill_pixels_cent_sym(raw, dimension)
     elif vertical_sym or horizontal_sym:
-        raw = id_hash(name, mid*dimension, min_fill, max_fill, use_color)
+        raw = id_hash(name, mid * dimension, min_fill, max_fill, use_color)
         if vertical_sym:
             pic = fill_pixels_vert_sym(raw, dimension)
         else:
             pic = fill_pixels_hori_sym(raw, dimension)
     else:
-        raw = id_hash(name, dimension*dimension, min_fill, max_fill, use_color)
+        raw = id_hash(name, dimension * dimension, min_fill, max_fill,
+                      use_color)
         pic = fill_pixels(raw, dimension)
     if isinstance(bg_color, int):
         bg_color = raw['colors'][bg_color]
     if isinstance(tile_color, int):
         tile_color = raw['colors'][tile_color]
-    im = Image.new('RGBA', (canvas_size*draw_scale, canvas_size*draw_scale))
+    im = Image.new('RGBA', (canvas_size * draw_scale,
+                            canvas_size * draw_scale))
     draw = ImageDraw.Draw(im)
-    draw.rectangle((0, 0, canvas_size*draw_scale, canvas_size*draw_scale),
+    draw.rectangle((0, 0, canvas_size * draw_scale, canvas_size * draw_scale),
                    fill=tuple(bg_color))
     for x in range(dimension):
         for y in range(dimension):
             if pic[y][x] == 1:
-                x0 = (x*tile_width) + tile_padding + image_padding
-                y0 = (y*tile_width) + tile_padding + image_padding
+                x0 = (x * tile_width) + tile_padding + image_padding
+                y0 = (y * tile_width) + tile_padding + image_padding
                 draw.rectangle( 
-                    (x0*draw_scale, y0*draw_scale,
-                     (x0 + tile_size)*draw_scale-1,
-                     (y0 + tile_size)*draw_scale-1),
+                    (x0 * draw_scale, y0 * draw_scale,
+                     (x0 + tile_size) * draw_scale - 1,
+                     (y0 + tile_size) * draw_scale - 1),
                     fill=tuple(tile_color)
                 )
     del draw
